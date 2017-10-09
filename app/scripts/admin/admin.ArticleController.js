734ac209df1bcc8a125823f5c6149282ef9a587f
3sanket3 <sanket@redsoftware.in>
2017-10-09 18:19:23 +0530

    product save button disable and confirmation on state change

--- app/scripts/admin/admin.ArticleController.js
+++ app/scripts/admin/admin.ArticleController.js
@@ -1,6 +1,6 @@
 angular.module('amcomanApp')
-    .controller('AdminArticleController', ['$scope', '$state','AdminArticleService',
-        function ($scope, $state,AdminArticleService) {
+    .controller('AdminArticleController', ['$scope', '$state', 'AdminArticleService',
+        function ($scope, $state, AdminArticleService) {
 
             //can be part of component
             $scope.mode = undefined;
@@ -10,14 +10,14 @@ angular.module('amcomanApp')
                     $scope.isSaveAllowed = true;
                     $scope.isEditAllowed = false;
                     $scope.isNewAllowed = false;
-                    $scope.isDeleteAllowed =false;
+                    $scope.isDeleteAllowed = false;
                     break;
                 case 'app.adminEditArticle':
                     $scope.mode = 'Edit';
                     $scope.isSaveAllowed = true;
                     $scope.isEditAllowed = false;
                     $scope.isNewAllowed = true;
-                    $scope.isDeleteAllowed =true;
+                    $scope.isDeleteAllowed = true;
                     break;
 
                 default:
@@ -25,16 +25,35 @@ angular.module('amcomanApp')
                     $scope.isSaveAllowed = false;
                     $scope.isEditAllowed = true;
                     $scope.isNewAllowed = true;
-                    $scope.isDeleteAllowed =true;
+                    $scope.isDeleteAllowed = true;
             }
             //--end--//
 
-            
+
             $scope.product = {};
             $scope.product.imgAlt = "asdad";
-            AdminArticleService.product.get({id: 31741},function(data){
+            AdminArticleService.product.get({ id: 31741 }, function (data) {
                 console.log(JSON.stringify(data));
-            },function(error){
+            }, function (error) {
                 console.log(JSON.stringify(error));
+            });
+
+
+            $scope.saveProduct = function(product){
+                //call api to save peroduct
+
+                $scope.product = {};
+                $scope.form.$dirty = false;
+                $state.go("app.aboutus");
+                
+            }
+            //To ask confirmation is user sure to leave the page
+            $scope.$on('$stateChangeStart', function (event) {
+                if ($scope.form.$dirty) {
+                    var answer = confirm("You may have pending changes, are you sure you want to leave this page?")
+                    if (!answer) {
+                        event.preventDefault();
+                    }
+                }
             });
         }]);
